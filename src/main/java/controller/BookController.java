e3eb7f008b84c84a3e3736a12ae1132809e1fa38
Srdn <giuseppespanedda@outlook.it>
2023-10-04 22:19:55 +0200

    aaa

--- src/main/java/controller/BookController.java
+++ src/main/java/controller/BookController.java
@@ -44,7 +44,7 @@ public class BookController {
         return null;
     }
 
-    // Endpoint per eliminare un libro
+    // Endpoint per eliminare un libr0!
     @DeleteMapping("/{id}")
     public void deleteBook(@PathVariable Long id) {
         bookRepository.deleteById(id);
